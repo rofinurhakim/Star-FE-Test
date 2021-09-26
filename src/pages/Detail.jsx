3adb05719754608d3de874099ecfe4b5e03cdb9f
rofinurhakim <rofinurhakim8@gmmail.com>
2021-09-26 20:26:38 +0700

    Final

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -5,6 +5,7 @@ import vector3 from '../assets/img/Vektor3.png'
 import vector4 from '../assets/img/Vector4.png'
 import vector5 from '../assets/img/Vector5.png'
 import '../assets/style/detail.scss'
+import Gr from '../assets/img/Gr.png'
 
 
 
@@ -15,30 +16,36 @@ const Detail = () => {
             <div className="detailpage">
             <div className="container detailPage">
                 <div className="row country">
-                    <div className="col-6 section-1">
+                    <div className="col-md-6 section-1">
                         <h1>ROMA, ITALIA</h1>
                         <p>te veniam, nonummy consequat. luptatum aliquip iriure ipsum et sit dolor et facilisi. <br/> volutpat. erat suscipit commodo vulputate consequat, qui wisi vero facilisis <br/> tincidunt amet, vel accumsan dolore exerci molestie elit, nulla ut eu dignissim feugait <br/> feugiat quis autem nibh ex eros at nostrud tation enim dolor duis praesent sed</p>
                         <p>ea feugait delenit consectetuer blandit suscipit minim erat dolore dignissim iusto <br/> amet, volutpat. Lorem consequat, feugiat laoreet sed illum zzril et autem nostrud wisi <br/> sit nulla ut aliquam ad facilisi.</p>
-                    </div>
-                    <div className="row mt-4 logo ">
-                        <div className="col-1">
-                            <img src={vector1} alt="" />
+                        <div className="row mt-4 logo ">
+                            <div className="col-1">
+                                <img src={vector1} alt="" />
                         </div>
-                        <div className="col-1">
+                         <div className="col-md-1">
                             <img src={vector2} alt="" />
                         </div>
-                        <div className="col-1">
+                            <div className="col-md-1">
                             <img src={vector3} alt="" />
                         </div>
-                        <div className="col-1">
+                            <div className="col-md-1">
                             <img src={vector4} alt="" />
                         </div>
-                        <div className="col-1">
+                            <div className="col-md-1">
                             <img src={vector5} alt="" /> 
                         </div>
+                    
                     </div>
+                    </div>
+                    <div className="col-md-6 contentRight">
+                        <img src={Gr} alt="" />
+
+                    </div>
+                    
                 </div>
-            </div>
+            </div> 
             </div>
            
         </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,11 +15,11 @@ const HomePage = () => {
     const history = useHistory()
 
     let slides = [
-        <img  src="https://picsum.photos/800/300/?random" alt="1" />,
-        <img  src="https://picsum.photos/800/301/?random" alt="2" />  ,
-        <img  src="https://picsum.photos/800/302/?random" alt="3" />  ,
-        <img  src="https://picsum.photos/800/303/?random" alt="4" />  ,
-        <img src="https://picsum.photos/800/304/?random" alt="5" />   ];
+        <img  onClick={() => history.push('/Detail')}  src="https://picsum.photos/800/300/?random" alt="1" />,
+        <img  onClick={() => history.push('/Detail')} src="https://picsum.photos/800/301/?random" alt="2" />  ,
+        <img  onClick={() => history.push('/Detail')} src="https://picsum.photos/800/302/?random" alt="3" />  ,
+        <img  onClick={() => history.push('/Detail')} src="https://picsum.photos/800/303/?random" alt="4" />  ,
+        <img  onClick={() => history.push('/Detail')} src="https://picsum.photos/800/304/?random" alt="5" />   ];
 
     return (
         <> 
